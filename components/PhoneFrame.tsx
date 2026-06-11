41b2bcc88a7c4047ecac621b0b77bee3be772ea9
daisanity-svg <daisanity@icloud.com>
2026-06-11 19:58:19 +0800

    Refine kid phone frame layout

--- components/PhoneFrame.tsx
+++ components/PhoneFrame.tsx
@@ -2,10 +2,8 @@ import type { ReactNode } from 'react';
 
 export function PhoneFrame({ children }: { children: ReactNode }) {
   return (
-    <main className="safe-screen mx-auto flex w-full max-w-[430px] min-w-0 flex-col overflow-x-hidden px-4">
-      <div className="flex min-h-[calc(100dvh-128px)] min-w-0 flex-col gap-4 overflow-x-hidden">
-        {children}
-      </div>
+    <main className="kid-shell">
+      <div className="kid-stack">{children}</div>
     </main>
   );
 }
