3824ef21cc0214806cb50047255f93ebd3f51a4e
daisanity-svg <daisanity@icloud.com>
2026-06-12 08:06:18 +0800

    Show today drawn card on reward page

--- app/reward/page.tsx
+++ app/reward/page.tsx
@@ -2,6 +2,7 @@ import { KidBottomNav } from '@/components/KidBottomNav';
 import { KidTopBar } from '@/components/KidTopBar';
 import { PhoneFrame } from '@/components/PhoneFrame';
 import { RewardDrawPanel } from '@/components/RewardDrawPanel';
+import { getTodayDrawnReward } from '@/lib/data/rewards';
 
 type RewardPageProps = {
   searchParams?: Promise<{
@@ -12,11 +13,12 @@ type RewardPageProps = {
 export default async function RewardPage({ searchParams }: RewardPageProps) {
   const params = await searchParams;
   const practiceRecordId = params?.practice_record_id;
+  const todayDraw = await getTodayDrawnReward(practiceRecordId);
 
   return (
     <PhoneFrame>
       <KidTopBar title="今日獎勵" backHref="/practice" backLabel="練習" rightLabel="🎁" />
-      <RewardDrawPanel practiceRecordId={practiceRecordId} />
+      <RewardDrawPanel practiceRecordId={practiceRecordId} initialResult={todayDraw} />
       <KidBottomNav />
     </PhoneFrame>
   );
